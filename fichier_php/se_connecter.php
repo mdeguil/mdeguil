ebcc1e9a659bb53966fb1b9fb8af32dcd09fb265
mdeguil <deguil.mateo@gmail.com>
2024-11-29 09:48:27 +0100

    connexion effectuer + redirection

--- fichier_php/se_connecter.php
+++ fichier_php/se_connecter.php
@@ -5,16 +5,28 @@ $dbh = new PDO('mysql:host=localhost;dbname=Site_Web_Aunis_Freeware', 'mysql', '
 $identification = $_POST['identification'];
 $mdp = $_POST['mdp'];
 
-$req = ('SELECT COUNT(*) FROM admin WHERE identifiant LIKE :identification OR email LIKE :identification;');
+echo $identification;
+echo "<br>";
+echo $mdp;
+echo "<br>";
+
+$req = ('SELECT COUNT(*) FROM admin WHERE mdp = :mdp AND identifiant LIKE :identification OR email LIKE :identification;');   
 $res = $dbh->prepare($req);
-$res->execute(['identification' => $identification]);
+$res->bindParam(":identification", $identification);
+$res->bindParam(":mdp", $mdp);
+$res->execute();
 
 $resultat = $res->fetchColumn() > 0;
+
+echo $resultat;
+echo "<br>";
 
 // Affichage du résultat
 if ($resultat) {
     echo "L'utilisateur existe.";
+    echo '<meta http-equiv="refresh" content="2; URL=accueil.php">';
 } else {
     echo "L'utilisateur n'existe pas.";
+    echo '<meta http-equiv="refresh" content="2; URL=connexion.php">';
 }
 ?>
