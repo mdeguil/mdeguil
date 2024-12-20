7c1b6aa3b7e4a6e661c4e0993ee978f538601379
mdeguil <deguil.mateo@gmail.com>
2024-12-20 09:41:57 +0100

    Conf mot de passe

--- fichier_php/compte.php
+++ fichier_php/compte.php
@@ -7,22 +7,28 @@
     $prenom = $_POST['prenom'];
     $email = $_POST['email'];
     $mdp = $_POST['mdp'];
-    $mdp = hash("sha256", $mdp);
+    $confmdp = $_POST['Confmdp'];
     $fonction = "V";
 
-    $sql = $dbh-> prepare('INSERT INTO admin (identifiant, fonction, nom, prenom, email, mdp ) VALUES (:identifiant, :fonction, :nom, :prenom, :email, :mdp)');
+    if ($mdp !== $confmdp){
+        echo "Les mots de passe ne correspondent pas !! ";
+        echo '<meta http-equiv="refresh" content="5; URL=creation_de_compte.php">';
+    }else {
+        $sql = $dbh-> prepare('INSERT INTO admin (identifiant, fonction, nom, prenom, email, mdp ) VALUES (:identifiant, :fonction, :nom, :prenom, :email, :mdp)');
 
-    $sql->bindParam(":identifiant", $identifiant);
-    $sql->bindParam(":fonction", $fonction);
-    $sql->bindParam(":nom", $nom);
-    $sql->bindParam(":prenom", $prenom);
-    $sql->bindParam(":email", $email);
-    $sql->bindParam(":mdp", $mdp);
+        $sql->bindParam(":identifiant", $identifiant);
+        $sql->bindParam(":fonction", $fonction);
+        $sql->bindParam(":nom", $nom);
+        $sql->bindParam(":prenom", $prenom);
+        $sql->bindParam(":email", $email);
+        $mdp = hash("sha256", $mdp);
+        $sql->bindParam(":mdp", $mdp);
 
-    // redirection vers une autre page
-    if ($sql->execute()) {
-        header("Location: accueil.php");
-    } else {
-        header("Location: creation_de_compte.php");
-    };
+        // redirection vers une autre page
+        if ($sql->execute()) {
+            header("Location: accueil.php");
+        } else {
+            header("Location: creation_de_compte.php");
+        };
+    }
 ?>
