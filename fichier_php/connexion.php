e5ea66293e857b1c82b7263aa085c08b2426077d
mdeguil <deguil.mateo@gmail.com>
2024-12-20 17:08:30 +0100

    ajout include page connexion

--- fichier_php/connexion.php
+++ fichier_php/connexion.php
@@ -8,67 +8,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </head>
 <body class="container">
-    <header>
-        <nav class="navbar navbar-expand-sm bg-success navbar-dark">
-            <div class="container-fluid">
-                <img src="../img/logo_af_2.jpeg" alt="Logo d'accueil" style="width:50px;" class="rounded-pill">
-            
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active " href="accueil.php"> Accueil </a>
-                    </li>
-                    <li class="nav-item"> 
-                        <a class="nav-link active" href="actualites.php"> Actualités </a>
-                    </li>
-                    <li class="nav-item"> 
-                        <a class="nav-link active" href="nos_applications.php">Nos applications </a>
-                    </li>
-                    <li class="nav-item"> 
-                        <a class="nav-link active" href="">Blog </a>
-                    </li>
-                    <li class="nav-item"> 
-                        <a class="nav-link active" href="connexion.php">Connexion </a>
-                    </li>
-                </ul>
-            </div>
-        </nav> 
-    </header>
-    <br>
+    <?php include 'entete.php';?>
     <main class="container px-4">
-        <!-- Carouselle dynamique en PHP avec recherche d'image automatique dans un dossier -->
-<div id="carouselExample" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-        <?php
-        $images = scandir('../aunis_photos');
-        $i = 0;
-
-        foreach ($images as $image) {
-            // Ignorer les entrées spéciales '.' et '..' et vérifier que c'est un fichier image
-            if ($image !== '.' && $image !== '..' && preg_match('/\.(jpg|jpeg|png|gif)$/i', $image)) {
-                ?>
-                <div class="carousel-item <?php if ($i == 0) echo 'active'; ?>">
-                    <img src="../aunis_photos/<?php echo $image; ?>" class="d-block w-100" alt="Image <?php echo $i + 1; ?>">
-                </div>
-                <?php
-                $i++;
-            }
-        }
-        ?>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only"></span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExample" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only"></span>
-    </a>
-</div>
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
+        <?php include 'carouselle.php';?>
         <div class="container-md">
             <article class="row gx -5">
                 <div class="col p-3">
@@ -103,25 +45,7 @@
             </article>
         </div>
     </main>
-    <br>
-    <footer class="footer">
-        <div class="row border border-success bg-success bg-opacity-50">
-            <div class="col align-self-center">
-                <a class="nav-link disabled black">Copyright Aunis Freeware</a>
-            </div>
-            <div class="col align-self-center">
-                <a class="nav-link active" href="mentions_legales.php">Mentions légales</a>
-            </div>
-            <div class="col align-self-center">
-                <img src="../img/facebook.jpg" alt="facebook" style="width:50px;">
-            </div>
-            <div class="col align-self-center">
-                <img src="../img/instagram.png" alt="instagram" style="width:50px;">
-            </div>
-            <div class="col align-self-center">
-                <a href="contactez-nous.php"><button class="btn border-secondary text-secondary">Contactez nous</button></a>
-            </div>
-        </div>
-    </footer>   
+    <?php include 'piedDePage.php';?>
+    <?php include 'lienScript.php';?>
 </body>
 </html>
