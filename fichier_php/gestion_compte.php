90ff2877f516a5e6a51c5a8d0dd1a64677e60c74
mdeguil <deguil.mateo@gmail.com>
2024-12-20 17:21:04 +0100

    modif page gestion de compte

---
+++ fichier_php/gestion_compte.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>BTSSIO_WEB</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body class="container">
+    <?php include 'entete.php';?>  
+    <main class="container px-4"> 
+        <?php include 'carouselle.php';?> 
+        <br>
+        <h1 class="text-center"> En construction </h1>   
+        <br>
+        
+    </main>
+    <?php include 'piedDePage.php';?>
+    <?php include 'lienScript.php';?>   
+</body>
+</html>
